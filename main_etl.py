f833cd46b1f15a361301ff80ee041bf1ae73844f
aboutas <athanasiosmpoutas@gmail.com>
2025-05-16 22:25:34 +0300

    Add mongoDb

--- main_etl.py
+++ main_etl.py
@@ -5,6 +5,16 @@ import logging
 import os
 import json
 from file_helpers import ensure_directory_exists, load_config, save_to_file
+from pymongo import MongoClient
+from pymongo_get_database import get_database
+
+def get_database(): 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb+srv://user:pass@cluster.mongodb.net/myFirstDatabase"
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['straming_etl']
 
 # Configure basic logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -17,6 +27,9 @@ if __name__ == "__main__":
         input_data_path = config["input_data_path"]
         save_path = config["save_path"]
         applied_rules_path = config["applied_rules_path"]
+
+        # Get the database
+        dbname = get_database()
         
         # Ensure the directory where parsed data will be saved exists
         ensure_directory_exists(os.path.dirname(save_path))
@@ -32,9 +45,10 @@ if __name__ == "__main__":
         rule_manager_transform = RuleManagerTransform(schema_handler.schema_registry, selected_rules , verbosity)
 
         data_stream = env.from_collection([json.dumps(item) for item in json_inputs])
-        transformed_stream = data_stream.map(rule_manager_transform)
+        transformed_data = data_stream.map(rule_manager_transform)
 
-        transformed_stream.map(lambda data: save_to_file(data, save_path))
+        transformed_data.map(lambda data: save_to_file(data, save_path))
+        transformed_data = dbname["transformed_data"]    
 
         env.execute("Dynamic JSON to File with Flink")
         logging.info("Transforamtions completed saccesfully! The resulst has store in a json file.")
